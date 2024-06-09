b97e2331c541af064567ae185c7dcbbae3538206
Gustavo-Pardinho <gustavo.pardinho@sptech.school>
2024-06-09 00:23:24 -0300

    organizando de pastas, arquivos e linkando as páginas corretamente

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ process.env.AMBIENTE_PROCESSO = "producao"
 var PORTA = process.env.AMBIENTE_PROCESSO == "producao" ? 80:3333; 
 
 app.use(express.static(path.join(__dirname, 'Site-institucional')));
-app.use(express.static(path.join(__dirname, 'dashboard')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'img')));
+app.use(express.static(path.join(__dirname, 'js')));
 
 const indexRoute = require('./routes/index');
 app.use('/', indexRoute);
 
 app.listen(PORTA, function () {
-    console.log(`Servidor ativo na porta: ${PORTA}`);
+    console.log(`Servidor ativo: http://localhost:${PORTA}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,22 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/cadastro', (req, res) => {
-    res.sendFile(path.join(__dirname, '../cadastro.html'));
+    res.sendFile(path.join(__dirname, '../public/cadastro.html'));
 });
 
 router.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, '../login.html'));
+    res.sendFile(path.join(__dirname, '../public/login.html'));
 });
 
 router.get('/dash', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dashboard/dash.html'));
+    res.sendFile(path.join(__dirname, '../public/dash.html'));
 });
 router.get('/dashGeral', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dashboard/dashGeral.html'));
+    res.sendFile(path.join(__dirname, '../public/dashGeral.html'));
 });
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
